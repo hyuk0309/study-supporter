2533c2688851a91db8c126dd4616ce8c2bc8f41e
hyuk0309 <bahngeh@naver.com>
2022-12-31 09:07:30 +0900

    feat : init project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Study Supporter
+
+## Intro
+Study를 진행하면서 필요성을 느낀 기능을 직접 개발하는 프로젝트
+
+## User Guid (WIP)
+
+## Feature
+
+- Choose Presenter
---
+++ src/main/kotlin/com/elvis/studysupporter/StudySupporterApplication.kt
@@ -0,0 +1,11 @@
+package com.elvis.studysupporter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudySupporterApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudySupporterApplication>(*args)
+}
