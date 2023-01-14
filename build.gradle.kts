c4925dbe71770c84c9515bf5417a3364c70de61a
hyuk0309 <bahngeh@naver.com>
2023-01-14 23:10:20 +0900

    chore : add kotest, spring jpa dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
+    kotlin("plugin.jpa") version "1.7.22"
 }
 
 group = "com.elvis"
@@ -18,11 +19,19 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     runtimeOnly("com.h2database:h2")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation ("io.kotest:kotest-runner-junit5:5.5.4")
+    testImplementation ("io.kotest:kotest-assertions-core:5.5.4")
+    testImplementation ("io.kotest:kotest-property:5.5.4")
 }
 
 tasks.withType<KotlinCompile> {
