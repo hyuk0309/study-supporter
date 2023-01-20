5d916ff218956a0b215a1f1b4abef129d78429e0
hyuk0309 <bahngeh@naver.com>
2023-01-20 20:19:10 +0900

    chore : add spring extension dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     testImplementation ("io.kotest:kotest-runner-junit5:5.5.4")
     testImplementation ("io.kotest:kotest-assertions-core:5.5.4")
     testImplementation ("io.kotest:kotest-property:5.5.4")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
 }
 
 tasks.withType<KotlinCompile> {
