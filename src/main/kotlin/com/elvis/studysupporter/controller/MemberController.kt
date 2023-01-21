0ce0ff7456bdd887eac05d7d85c0a736d93ac99a
hyuk0309 <bahngeh@naver.com>
2023-01-21 19:07:42 +0900

    feat : design modal form

--- src/main/kotlin/com/elvis/studysupporter/controller/MemberController.kt
+++ src/main/kotlin/com/elvis/studysupporter/controller/MemberController.kt
@@ -5,11 +5,7 @@ import com.elvis.studysupporter.controller.dto.MembersResponse
 import com.elvis.studysupporter.domain.MemberService
 import com.elvis.studysupporter.domain.dto.MemberResult
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/members")
